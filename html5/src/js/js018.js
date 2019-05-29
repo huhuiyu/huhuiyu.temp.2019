0daed4fedec51c30807f1081d9c839c622454ff8
huhuiyu <1069306849@qq.com>
2019-05-29 11:06:50 +0800

    function

--- html5/src/js/js018.js
+++ html5/src/js/js018.js
@@ -29,3 +29,43 @@ setInterval(function() {
 }, 1000);
 
 showTime();
+
+//用户登陆========================
+//定义重置表单的动作，重置到初始化的状态
+//也就是重置和初始化是一样，所以定义function
+function reset() {
+  let txtName = document.getElementById('txtName');
+  let txtPwd = document.getElementById('txtPwd');
+  txtName.value = 'admin';
+  txtPwd.value = '';
+  txtPwd.focus();
+}
+
+reset();
+
+let btnReset = document.getElementById('btnReset');
+btnReset.addEventListener('click', function() {
+  reset();
+});
+
+//总结
+//function格式：
+//function 名称(){
+//代码块
+//}
+//作用是将名称和代码块做一个绑定
+//调用方式:
+//名称();
+//效果是执行名称对应的代码块
+//function最大的优点就是相同的代码逻辑封装起来
+//方便反复调用，避免的大量相同的代码反复复制
+//而且修改起来也更加方便
+
+//作业1，将蝙蝠侠大战小丑改版成function
+//  相同的代码不在反复出现
+//作业2，将时间显示格式前导补零
+//   且：如果时间是0-12点，要显示上午
+//          时间是13-23点，要显示下午
+//作业3，蝙蝠侠大战小丑，提供暴击功能
+//    实现方式为，随机产生1-5的随机数
+//    如果结果是5就表示暴击，将伤害值加倍
